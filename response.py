2d7535cccfdc0e46b2209497c4ab9e595f691588
Your Name <you@example.com>
2020-09-29 02:02:12 +0300

    Expanded Defaults, Restructured Not Default Response

--- response.py
+++ response.py
@@ -1,10 +1,14 @@
 class Response:
 
-    def __init__(self,request_headers,response_body):
+    def __new__(self,request_headers,response_body,**kwargs):
         self.body = response_body
 
-    def encode(self):
-        return 'HTTP/1.1 200 OK\r\nServer: PyServer\r\n\r\n{}\r\n\r\n'.format(self.body).encode()
+        class RespBytes(bytes):
+
+            def __new__(cls,*args,**kwargs):
+                return 'HTTP/1.1 200 OK\r\n\r\n{}\r\n\r\n'.format(self.body).encode()
+
+        return RespBytes()
 
 
 class DefaultResponse:
@@ -12,16 +16,25 @@ class DefaultResponse:
     class BadRequest(bytes):
 
         def __new__(cls,*args,**kwargs):
-            return super().__new__(cls,'HTTP/1.1 400 Not Found\r\n\r\n',encoding='utf-8')
+            return super().__new__(cls,'HTTP/1.1 400 Bad Request\r\n\r\n',encoding='utf-8')
 
     class Unauthorized:
 
         def __new__(cls,*args,**kwargs):
             return super().__new__(cls,'HTTP/1.1 401 Unauthorized\r\n\r\n',encoding='utf-8')
 
+    class Forbidden:
+
+        def __new__(cls,*args,**kwargs):
+            return super().__new__(cls,'HTTP/1.1 403 Forbidden\r\n\r\n',encoding='utf-8')
+
     class NotFound(bytes):
 
         def __new__(cls,*args,**kwargs):
             return super().__new__(cls,'HTTP/1.1 404 Not Found\r\n\r\n',encoding='utf-8')
 
+    class Method_Not_Allowed(bytes):
+
+        def __new__(cls,*args,**kwargs):
+            return super().__new__(cls,'HTTP/1.1 405 Method Not Allowed\r\n\r\n',encoding='utf-8')
 
